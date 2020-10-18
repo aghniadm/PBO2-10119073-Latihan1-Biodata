f562ecf13eec67b8e73cfe85a3267fc69b62a4aa
aghniadm <aghniadewii@gmail.com>
2020-10-18 15:53:54 +0700

    PBO2-10119073-Latihan1-Biodata

---
+++ src/pbo2/pkg10119073/latihan1/biodata/PBO210119073Latihan1Biodata.java
@@ -0,0 +1,27 @@
+package pbo2.pkg10119073.latihan1.biodata;
+
+/**
+ *
+ * @author 
+ * NAMA     : Aghnia Dewi Mahiranie
+ * KELAS    : PBO2
+ * NIM      : 10119073
+ * Deskripsi Program : Program ini berisi program untuk menampilkan perintah
+ * value ke layar
+ *
+ */
+public class PBO210119073Latihan1Biodata {
+
+    public static void main(String[] args) {
+        System.out.println("BIODATA" + "\n" + "----------------");
+        System.out.println("NAMA\t\t: Aghnia Dewi Mahiranie");
+        System.out.println("ALAMAT\t\t: Jalan Pelesiran");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 24 Januari 2002");
+        System.out.println("----------------");
+        System.out.println("HOBI\t\t: Menggambar");
+        System.out.println("MAKES\t\t: Ayam bakar");
+        System.out.println("MIKES\t\t: Es jeruk");
+    }
+    
+}
